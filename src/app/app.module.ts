c2a89b1bbfba56ee8008899d961d356807b07876
Jeremiah Tan <axel.max97@gmail.com>
2023-10-19 02:30:24 +0800

    Add : footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,16 @@ import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzCarouselModule } from 'ng-zorro-antd/carousel';
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzInputModule} from 'ng-zorro-antd/input'
+import { NzDividerModule} from 'ng-zorro-antd/divider'
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    FooterComponent,
     HomeComponent,
   ],
   imports: [
@@ -23,6 +26,7 @@ import { AppRoutingModule } from './app-routing.module';
     NzCarouselModule,
     NzInputModule,
     NzCardModule,
+    NzDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
