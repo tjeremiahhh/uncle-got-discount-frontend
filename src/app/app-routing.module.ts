dbbcac86d7dedb43dc3060037ad9c7caa9a673d3
Jeremiah Tan <axel.max97@gmail.com>
2023-10-19 01:02:15 +0800

    Add : home component

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: '**', redirectTo: '' },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCarouselModule } from 'ng-zorro-antd/carousel';
+import { NzInputModule} from 'ng-zorro-antd/input'
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     NzButtonModule,
+    AppRoutingModule, 
+    NzCarouselModule,
+    NzInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
