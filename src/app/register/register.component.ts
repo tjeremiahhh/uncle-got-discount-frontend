4b17e593e59c7a389334e03faddd605d62a1d5a0
tjeremiahhh <axel.max97@gmail.com>
2023-10-19 16:59:05 +0800

    Improve (Register) : route to log in

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NzModalRef, NzModalService } from 'ng-zorro-antd/modal';
+import { AuthenticateComponent } from '../authentication/authenticate/authenticate.component';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private modalService : NzModalService,
+    private modalRef : NzModalRef,
+  ) { }
 
   ngOnInit() {
+  }
+
+  onLogIn() {
+    this.modalService.create({
+      nzContent: AuthenticateComponent,
+      nzWidth: 800,
+      nzFooter: null,
+      nzBodyStyle: {
+        'height': '500px',
+        'padding': '0'
+      }
+    })
+
+    this.modalRef.close();
   }
 
 }
