7f5c60a07f3c8aba348dae339d86ce50a2938526
tjeremiahhh <axel.max97@gmail.com>
2023-10-19 16:44:16 +0800

    Add : register component

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NzModalService } from 'ng-zorro-antd/modal';
+import { RegisterComponent } from 'src/app/register/register.component';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private modalSevice: NzModalService,
+  ) { }
 
   ngOnInit() {
+  }
+
+  onRegister() {
+    this.modalSevice.create({
+      nzContent: RegisterComponent,
+      nzWidth: 800,
+      nzFooter: null,
+      nzBodyStyle: {
+        'height': '500px',
+        'padding': '0'
+      }
+    })
   }
 
 }
