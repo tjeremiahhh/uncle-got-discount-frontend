2dbb34ed423c66cb2da3684ddb04551da2fc2989
tjeremiahhh <axel.max97@gmail.com>
2023-10-19 18:37:23 +0800

    Add (Register) : register api

---
+++ src/app/register/register.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { RegisterRequest } from "./model/register.model";
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class RegisterService {
+    baseUrl = "https://gew0xe9lag.execute-api.ap-southeast-1.amazonaws.com/Test1/authentication/"
+
+    constructor(private http : HttpClient) {}
+
+    register(registerRequest : RegisterRequest): Observable<any> {
+        return this.http.post<any>(this.baseUrl + 'register/', registerRequest);
+    }
+}
