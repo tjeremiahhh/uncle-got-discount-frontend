943828ffad9d22cf0347ad5f653f74521ee5147e
Jeremiah <jeremiah-tan@hotmail.com>
2023-10-22 22:05:02 +0800

    Fix: Slider on home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,18 +8,28 @@ import { HomeService } from './home.service';
   styleUrls: ['./home.component.less']
 })
 export class HomeComponent implements OnInit {
-  businessListings : BusinessListing[] = [];
+  businessListings: BusinessListing[] = [];
 
   constructor(
-    private homeService : HomeService,
+    private homeService: HomeService,
   ) { }
 
   ngOnInit() {
     this.homeService.getAllBusinessListings().subscribe({
-      next: (res : any) => {
+      next: (res: any) => {
         this.businessListings = res;
       }
     })
   }
-  
+
+  slideChange(direction: any, id: string) {
+    const slider = document.getElementById(id);
+    const sliderItems = slider?.getElementsByClassName('custom-list-item');
+
+    if (slider && sliderItems) {
+      const sliderItem = sliderItems[0].scrollWidth;
+      slider.scrollLeft += sliderItem * direction;
+    }
+  }
+
 }
