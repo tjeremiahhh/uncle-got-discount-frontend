c95a9b21441e0a08c6446b32bbb0b8a214bed2fb
xaviertanwc <66908992+xaviertanwc@users.noreply.github.com>
2023-10-23 14:02:31 +0800

    ✨

--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -9,18 +9,11 @@ import { devMicroserviceUrl, prodMicroserviceUrl } from "src/environments/enviro
 })
 export class HomeService {
     businessListingUrl = "https://gew0xe9lag.execute-api.ap-southeast-1.amazonaws.com/api/business-listing/";
-    searchFilterUrl = "http://localhost:8080/api/search-filter/";
 
     constructor(private http : HttpClient) {}
 
     getAllBusinessListings(): Observable<any> {
         return this.http.get<any>(prodMicroserviceUrl.business_listing + 'get-all-business-listings');
-    }
-
-    searchByOutletName(outletName: string): Observable<ISearchResult[]> {
-        let params: HttpParams = new HttpParams().set("outletName", outletName);
-
-        return this.http.get<ISearchResult[]>(devMicroserviceUrl.search_filter + 'search-by-outlet-name', { params: params });
     }
 
     getBusinessListingDescriptionDetails(): Observable<any> {
